980f4453a4c339243580cd47d9c7da590caeb792
Nida Batool <nida711711@gmail.com>
2025-09-20 13:51:16 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */ images: {
     domains: ['res.cloudinary.com'], 
   },
+   api: {
+    bodyParser: {
+      sizeLimit: '10mb', // jitni limit chahiye set kar lo
+    },
+  },
+  
 };
 
 export default nextConfig;
